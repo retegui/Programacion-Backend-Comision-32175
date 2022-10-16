e9be2484326f8fdc0ef01133f15ec31035eacdbd
retegui <nahuelretegui@gmail.com>
2022-10-16 01:32:26 -0300

    Desafio Clases

--- clases.js
+++ clases.js
@@ -1,32 +1,38 @@
-class Contador{
-    constructor(Usuario){
+class Usuario{
+    constructor(nombre,apellido,libros,mascotas){
         this.nombre = nombre;
-        this.apellido = "Bogota";
-        this.libros = {"1985":"Orwell"};
-        this.mascotas = "Pupi";
+        this.apellido = apellido;
+        this.libros = libros;
+        this.mascotas = mascotas;
     }
 
     static cuentaGlobal = 0;
 
     getFullName (){
-        return this.nombre+ this.apellido;
+        return this.nombre + this.apellido;
     }
 
     addMascotas (){
-        return this.cuentaIndividual;
+        return this.mascotas;
     }
 
     countMascotas (){
-        return Contador.cuentaGlobal;
+        return count.mascotas;
     }
 
-    addLibros(){
-        this.cuentaIndividual++;
-        Contador.cuentaGlobal++;
+    addBook(){
+        this.libros++;
+        
     }
     getBookNames(){
         this.cuentaIndividual++;
-        Contador.cuentaGlobal++;
     }
 }
 
+const usuarioNahuel = new Usuario ("Nahuel","Retegui",[{nombre:'1984',autor:'George Orwell'},{nombre:'La naranja mecanica',autor:'Anthony Burgess'}],['gato','jabali']);
+
+console.log(usuarioNahuel);
+console.log(usuarioNahuel.getFullName());
+// console.log('Nahuel: ', usuarioNahuel.countMascotas());
+// console.log('Nahuel: ',usuarioNahuel.addBook());
+// console.log('Nahuel: ',usuarioNahuel.getBookNames());
