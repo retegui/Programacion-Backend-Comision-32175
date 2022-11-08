4136fef519c0163504bf6d600c8203a2d8f9a9b1
retegui <nahuelretegui@gmail.com>
2022-11-07 21:00:57 -0300

    Servidor con Express

--- servidor-express/contenedorProductos.js
+++ servidor-express/contenedorProductos.js
@@ -2,30 +2,105 @@ const fs = require("fs");
 
 
 class Contenedor{
-    constructor(nameFile){
-        this.fileName = nameFile;
+    constructor(name){
+        this.fileName = name;
     }
 
-   async getAll(){
+    async save(product){
+        try{
+            if(fs.existsSync(this.fileName)){
+                const productos = await this.getAll();
+                if(productos.length>0){
+                    const lastId = productos[productos.length-1].id+1;
+                    product.id= lastId;
+                    productos.push(product);
+                    await fs.promises.writeFile(this.fileName,JSON.stringify(productos,null,2));
+                } else{
+                    product.id=1;
+                    await fs.promises.writeFile(this.fileName,JSON.stringify([product],null,2));
+            } 
+            } else{
+                product.id=1;
+                await fs.promises.writeFile(this.fileName,JSON.stringify([product],null,2));}
+            
+        }catch(error){
+            return "El producto no pudo ser guardado";
+        }
+    }
+
+    async getAll(){
     try{
         const contenido = await fs.promises.readFile(this.fileName,"utf-8");
         if(contenido.length>0){
             const productos = JSON.parse(contenido);
             return productos;
         } else {
-            return "Archivo vacio";
+            return [];
         }        
     } catch(error) {
         return "El archivo no puede ser leido";
     }
    }
+
+   async getById(id){
+    try {
+        //obtener todos los productos.
+        const productos = await this.getAll();
+        //buscar nuestro producto por el id
+        const producto = productos.find(elemento=>elemento.id === id);
+        return producto;
+    } catch (error) {
+        return "El producto no se encuentra";
+    }
 }
+
+async deleteById(id){
+    
+    try {
+        const productos = await this.getAll();
+        const newProducts = productos.filter(elemento=>elemento.id !== id);
+        await fs.promises.writeFile(this.filename,JSON.stringify(newProducts,null,2));
+        return `El producto con el id ${id} fue elimnado`;
+    } catch (error) {
+        return "El elemento no puede ser eliminado"
+    }
+}
+
+getName(){
+    return this.filename;
+}
+}
+
+// const producto1={
+//       nombre: "Pokemon Ruby", 
+//       precio: 8614,
+//       img: "https://i.ebayimg.com/images/g/c1kAAOSwzwZgzAS4/s-l1600.jpg"
+           
+//     }
+  
+
+// const producto2={        
+   
+//         nombre: "Super Mario 64",
+//         precio: 6175,
+//         img: "https://i.ebayimg.com/images/g/4AMAAOSwf9JhIhMv/s-l640.jpg"
+        
+//     }
+   
+
 
 const manejadorProductos = new Contenedor ("productos.txt");
 console.log(manejadorProductos);
 const getData = async()=>{
+        // const productos = await manejadorProductos.getAll();
+        // console.log("productos",productos);
+        // await manejadorProductos.save(producto1);
+        // await manejadorProductos.save(producto2);
         const productos = await manejadorProductos.getAll();
         console.log("productos",productos);
+        const productoEncontrado = await manejadorProductos.getById(1);
+        console.log("producto encontrado>", productoEncontrado);
+        await manejadorProductos.deleteById();
 }
 
 getData();
--- servidor-express/server.js
+++ servidor-express/server.js
@@ -8,19 +8,14 @@ const PORT = 8080;
 app.listen(PORT,()=>console.log(`El servidor esta configurado en el puerto ${PORT}`));
 const contenedorProductos = new Contenedor("./productos.txt");
 
-app.get("/productos",(request,response)=>{
-    contenedorProductos.GetAll()
+app.get("/productos",async (request,response)=>{
+    const productos = await contenedorProductos.getAll()
+    response.send(productos)
 })
 
-app.get("/productoRandom",(request,response)=>{
-    response.send({
-        id: 1,
-        nombre: "Super Mario 64",
-        consola: "Nintendo 64",
-        empresa: "Nintendo",
-        desc: "Super Mario 64 is a 1996 platform game for the Nintendo 64. Developed by Nintendo Entertainment Analysis and Development and published by Nintendo, it is the first Super Mario game to feature 3D gameplay, combining traditional Super Mario gameplay, visual style, and characters in a large open world. ",
-        precio: 6.175,
-        img: 'https://i.ebayimg.com/images/g/4AMAAOSwf9JhIhMv/s-l640.jpg',
-        cantidad:5
-    })
+app.get("/productoRandom",async (request,response)=>{
+    const productos = await contenedorProductos.getAll()
+    const random = Math.floor(Math.random()*productos.length+1)
+    const productoRandom = await contenedorProductos.getById(random)
+    response.send(productoRandom)
 })
