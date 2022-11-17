3599fdeb66b9fccda4fbc4cf29cbb1c5aa2dda2b
retegui <nahuelretegui@gmail.com>
2022-11-16 23:48:45 -0300

    Websockets

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const handlebars = require("express-handlebars");
 const path = require("path");
+const {Server} = require("socket.io");
+
 const Contenedor = require("./managers/contenedorProductos");
-
 const productsService = new Contenedor("productos.txt");
-
 const viewsFolder = path.join(__dirname,"views")
 
 
@@ -12,20 +12,25 @@ const app = express();
 app.listen(8080,()=>console.log("Servidor escuchando puerto 8080"));
 
 app.use(express.static(__dirname+"/public"));
-
-
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 
 app.engine("handlebars",handlebars.engine());
-
-
 app.set("views", viewsFolder);
-
-
 app.set("view engine", "handlebars");
 
+const io = new Server(server);
+io.on("connection", async(socket)=>{
+    console.log("nuevo cliente conectado");
+    socket.emit("productsArray", await productsService.getAll());
+
+    socket.on("newProduct", async(data)=>{
+        await productsService.save(data);
+
+        io.sockets.emit("productsArray", await productsService.getAll());
+    })
+})
 
 app.get("/",(req,res)=>{
     res.render("home")
